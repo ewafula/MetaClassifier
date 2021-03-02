10cba175ad088499a29a42cef39c37cf3e8a9370
ewafula <ewafula@users.noreply.github.com>
2021-03-02 09:03:41 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import pathlib
+from setuptools import setup, find_packages
+
+# setup.py directory also contains README.md
+setup_dir = pathlib.Path(__file__).parent
+README = (setup_dir / "README.md").read_text()
+
+
+setup(
+    name="MetaClassifier",
+    version="1.0.0",
+    description="MetaClassifier is an integrated pipeline for classifying and quantifying DNA metabarcoding data into taxonomy groups",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ewafula/MetaClassifier",
+    author='Eric Wafula',
+    author_email="ekw10@gmail.com",
+    license="GNU General Public License v3.0",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3.0",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=find_packages(exclude=["venv", "docs", "test", "bin", "db"]),
+    include_package_data=True,
+    install_requires=["pandas"],
+    entry_points={
+        "console_scripts": [
+            "metaclassifier=metaclassifier.__main__:main",
+            "process_reads=metaclassifier.process_reads.py:main",
+            "classify_reads.py=metaclassifier.classify_reads.py:main"
+        ],
+    },
+    zip_safe=True
+)
